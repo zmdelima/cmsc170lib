722c593b9c7733db8cb9f23116d66b793693d579
zmdelima <zmdelima@up.edu.ph>
2016-02-18 22:55:44 +0800

    Debug EightPuzzle Solver class

--- EightPuzzle/src/Solver.java
+++ EightPuzzle/src/Solver.java
@@ -4,22 +4,25 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.Point;
 import java.util.*;
+import java.lang.Math;
 
 public class Solver { //Solver class
-    public static HashMap <int, Point> gg = new HashMap<int, Point>();
-     
+    public static HashMap <Integer, Point> gg = new HashMap<Integer, Point>();
+    public static int i;
+    public static int j;
     public Solver (int[][] initConfig, int[][] initToggled) { //solver class constructor
         //creation of initial state and the frontier
-        State initialState = new State(initConfig, initToggled);
+        State initialState = new State(initConfig, initToggled,0,0,0,null);
         //initial state list
+        stateList openList = new stateList();
+        stateList closedList = new stateList(); 
         openList.add(initialState);
         //creation of currentState
         State bestState = initialState;
-        int i,j;
-        for(int i=0;i<3;i++){
-        	for(int j=0;j<3;j++){
-        		if(i==2 && j==2)gg.add(0,new Point(2,2));
-        		gg.add((i*3 +(j+1)),new Point(i,j));
+        for(i=0;i<3;i++){
+        	for(j=0;j<3;j++){
+        		if(i==2 && j==2)gg.put(0,new Point(2,2));
+        		gg.put((i*3 +(j+1)),new Point(i,j));
         	}
         }
         
@@ -32,7 +35,7 @@ public class Solver { //Solver class
             }
             else { //condition if the currentState does not have the solution yet
                 for ( Point p : getAction(bestState)) {
-                    s = bestState;
+                    State s = new State(bestState.config, bestState.legalActions, bestState.cost, bestState.dist, bestState.total, bestState);
                     openList.addLast(Result(bestState,p));
                 	
                 }            
@@ -41,98 +44,88 @@ public class Solver { //Solver class
         //creation of the Solution Frame and its button/light configurations
         JFrame solframe = new JFrame("SOLUTION");
         solframe.setPreferredSize(new Dimension(200,200));
-	   solframe.setResizable(false);  
-        /*
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-              	solframe.getContentPane().setLayout(new GridLayout(3,3));
-              	JButton qwe = new JButton();
-              	qwe.setBackground(bestState.legalActions[i][j] == 1? Color.YELLOW : Color.BLACK);
-              	qwe.setEnabled(false);
-              	solframe.getContentPane().add(qwe);
-            }
-        }*/
+	    solframe.setResizable(false);  
         solframe.pack();
         solframe.setVisible(true);
         
     }
 
-    public static class openList extends LinkedList<State> { //class for frontier
-    
-    }
-    
-    public static class closedList extends LinkedList<State> { //class for frontier
+    public static class stateList extends LinkedList<State> { //class for openlist and closedlist
     
     }
     
     public static class State { //This state is used to hold the current configuration of a state and its toggled coordinates
-        int[][] config = new int[3][3];
-        int[][] legalActions = new int[3][3];
-        int cost,dist,total;  
-        State parent;
-        public State (int[][] a, int[][] b) {
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-        			config = a[i][j];
-        			legalActions = b[i][j];        
-                }	
+        public static int[][] config = new int[3][3];
+        public static int[][] legalActions = new int[3][3];
+        public static int cost;
+        public static int dist;
+        public static int total;  
+        public static State parent;
+        
+        public State (int[][] a, int[][] b, int cost, int dist, int total, State s) {
+            for(i=0;i<3;i++){
+                for(j=0;j<3;j++){
+        			this.config[i][j] = a[i][j];
+        			this.legalActions[i][j] = b[i][j];        
+                }
             }
+            this.parent = s;
+            this.cost = cost;
+            this.dist = dist;
+            this.total = total;
         }
         
-        public static State toggle (State a, int x, int y, int vert, int hori) { //constructor of State class
+        public static State toggle (State s, int x, int y, int vert, int hori) { //constructor of State class
             int temp;
             //toggle coordinate x,y of the currentState's configuration
             temp = s.config[x][y];
             s.config[x][y] = s.config[vert][hori];
             s.config[vert][hori] = temp;
-		  s.setCost(s);
-		  s.setDist(s);
-		  s.setTotal(s);
+		    s.setCost();
+		    s.setDist();
+		    s.setTotal();
             //return the state
             return s;
         }
         
-        public static void setCost(s) {
+        public void setCost() {
         	this.cost = this.cost + 1;
         }
         
-        public static void setDist(s) {
-        	int total=0;
-        	for(int i=0;i<3;i++){
-        		for(int j=0;j<3;j++){
-        		int curr = this.config[i][j];
-        		if (curr==0)continue;
-        		total = total + abs(i-gg(curr).getX()) + abs(j-gg(curr).getY());
+        public void setDist() {
+        	int curr, total=0;
+        	
+        	for(i=0;i<3;i++){
+        		for(j=0;j<3;j++){
+            		curr = this.config[i][j];
+            		if (curr==0)continue;
+            		total = total + Math.abs(i-(gg.get(curr)).x) + Math.abs(j-(gg.get(curr)).y);
         		}
         	}
         	this.total=total;
         }
         
-        public static void setTotal(s) {
+        public void setTotal() {
         	this.total = this.cost + this.dist;
         }
         
-        public static void setParent(State s) {
-        	this.parent = s;
-        }
-        
-        public static void getLegalActions(int a, int b) {
+        public void getLegalActions(int a, int b) {
         	for(int i=0;i<3;i++){
         		for(int j=0;j<3;j++){
         			this.config[0][0]=0;
         		}
         	}
-        	if(b>0) this.config[a][b-1] = 1;
+          if(b>0) this.config[a][b-1] = 1;
           if(b<4) this.config[a][b+1] = 1;
           if(a>0) this.config[a-1][b] = 1;
           if(a<4) this.config[a+1][b] = 1;
         	
         }
         
-        public int[] static void getZero() {
+        public int[] getZero() {
         	int[] zero = new int[2];
-        	for(int i=0;i<3;i++){
-        		for(int j=0;j<3;j++){
+        	for(i=0;i<3;i++){
+        		for(j=0;j<3;j++){
         			if(this.config[i][j] == 0) {
         				zero[0]=i;
         				zero[1]=j;
@@ -140,6 +133,7 @@ public class Solver { //Solver class
         			}
         		}
         	}
+        	return zero;
         }
     }
     
@@ -155,8 +149,8 @@ public class Solver { //Solver class
     
     public static boolean GoalTest(State s) { //goalTest function for checking if solution has been found
         boolean res = true;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++) {
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++) {
             	 if(i==2 && j==2) break;
                 if(s.config[i][j] != (i*3 +(j+1))) res = false;        
             }
@@ -167,11 +161,11 @@ public class Solver { //Solver class
     public static State Result(State s, Point p) { 
     	//result function for getting the result state of an Action done in toggling Point p
         int[] zero = new int[2];
-        State resultState = new State(s.config, s.legalActions);
+        State resultState = new State(s.config, s.legalActions, s.cost, s.dist, s.total, s);
         zero = s.getZero();
         resultState = resultState.toggle(resultState, p.x, p.y,zero[0],zero[1]);
         resultState.getLegalActions(zero[0],zero[1]);
-        resultState.setParent(s);
+        //resultState.setParent(s);
         return resultState;
     }
 }
