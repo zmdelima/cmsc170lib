8c19e68dcd22847f31b31f356e670f94d8f75d55
zmdelima <zmdelima@up.edu.ph>
2016-03-17 13:34:26 +0800

    HotFix for BagOfWords and Documentation

--- BagOfWords/src/BagOfWords.java
+++ BagOfWords/src/BagOfWords.java
@@ -1,3 +1,8 @@
+//Author:Zydrick Jan M. Delima
+//S.N.:2013-42012
+//Date created: Mar 15-17,2016
+//Desc: Program with spam filtering system with the use of Naive Bayes classifier algorithm/method
+
 //packages to be used for BagOfWords
 import java.io.*;
 import java.util.*;
@@ -7,9 +12,11 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
 
 public class BagOfWords {
+    //global variables needed for the program
 	public final static LinkedHashMap<String,Integer> spam = new LinkedHashMap<String,Integer>();
 	public final static LinkedHashMap<String,Integer> ham = new LinkedHashMap<String,Integer>();
 	public static int total;
+	public static int tdSize;
 	public static int dSize1;
 	public static int dSize2;
 	public static int spamLines;
@@ -22,27 +29,31 @@ public class BagOfWords {
 		mainFrame.setPreferredSize(new Dimension(1000,400));
 		mainFrame.setResizable(false);
 		
+		//initialization of variables + filechooser
 		final JFileChooser fc = new JFileChooser();
-		total     = 0;
-		dSize1    = 0;
-		dSize2    = 0;
-		result    = 0;
-		spamLines = 0;
-		hamLines  = 0;
-		total     = 0;
+		total     = 0; //for total number of words in all tables
+		tdSize    = 0; //for total number of unique/dictionary words in all tables
+		dSize1    = 0; //for total number of unique/dicitonary words in the spam table
+		dSize2    = 0; //for total number of unique/dicitonary words in the ham table
+		result    = 0; //for spam probability computation result variable handler
+		spamLines = 0; //for the number of message lines in the spam text file input
+		hamLines  = 0; //for the number of message lines in the ham text file input
 		
+		//components for spam label display/status
 		JLabel spamLabel = new JLabel("Total words in Spam:");
 		final JTextField spamTotal = new JTextField("");
 		spamTotal.setPreferredSize(new Dimension(50,25));
 		spamTotal.setEditable(false);
 		spamTotal.setBackground(Color.WHITE);
 		
+		//components for ham label display/status
 		JLabel hamLabel = new JLabel("Total words in Ham:");
 		final JTextField hamTotal = new JTextField("");
 		hamTotal.setPreferredSize(new Dimension(50,25));
 		hamTotal.setEditable(false);
 		hamTotal.setBackground(Color.WHITE);
 		
+		//components for input textArea
 		JLabel inputLabel = new JLabel("Input:                  ");
 		final JTextArea input = new JTextArea("");
 		input.setWrapStyleWord(false);
@@ -65,6 +76,7 @@ public class BagOfWords {
 		bagT.setEditable(false);
 		bagT.setBackground(Color.WHITE);
 		
+		//string arrays for table column components
 		String[] colNames = {"Word","Frequency"};
 		String[] colNames2 = {"Message","Classification"};
 		String[][] data = null;
@@ -86,12 +98,15 @@ public class BagOfWords {
 		output.setEnabled(false);
 		final DefaultTableModel mod3 = (DefaultTableModel) output.getModel();
 		
+		//button for spam file input and propagation of spam table from browsed text file
 		JButton browseSpam = new JButton("Browse Spam.txt");
 		browseSpam.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
+			    //initialization of variables
 				dSize1 = 0;
 				for(String k : spam.keySet()){
 					total = total - spam.get(k);
+					tdSize = tdSize - 1;
 				}
 				spamLines=0;
 				spam.clear();
@@ -112,10 +127,13 @@ public class BagOfWords {
 		                    	if(i.matches("[w]*")){
 		                    		continue;
 		                    	}
-		                		total++;
-	                			dSize1++;
+		                		total = total + 1;
+	                			dSize1 = dSize1 + 1;
 		                		if ( !(spam.containsKey(i)) ) {
 		                			spam.put(i,1);
+		                			if(!(ham.containsKey(i))){
+		                			    tdSize = tdSize + 1;
+		                			}
 		                		} 
 		                		else if (spam.containsKey(i)) {
 		                			int temp = spam.get(i)+1;
@@ -130,7 +148,7 @@ public class BagOfWords {
 		            }
 		         }
 		         //initiate values
-		         bagS.setText(Integer.toString(spam.size()+ham.size()));
+		         bagS.setText(Integer.toString(tdSize));
 		         bagT.setText(Integer.toString(total));
 		         spamTotal.setText(Integer.toString(dSize1));
 		         for(String k : spam.keySet()){
@@ -139,14 +157,15 @@ public class BagOfWords {
 			}
 		});
 		
-		
-		
+		//button for spam file input and propagation of ham table from browsed text file
 		JButton browseHam = new JButton("Browse Ham.txt");
 		browseHam.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
+			    //initialization of variables
 				dSize2 = 0;
 				for(String k : ham.keySet()){
 					total = total - ham.get(k);
+					tdSize = tdSize - 1;
 				}
 				hamLines=0;
 				ham.clear();
@@ -171,6 +190,9 @@ public class BagOfWords {
 		                		dSize2++;
 		                		if ( !(ham.containsKey(i)) ) {
 		                			ham.put(i,1);
+		                			    if(!(spam.containsKey(i)){
+		                			        tdSize = tdSize + 1;
+		                			    }
 		                		} 
 		                		else if (ham.containsKey(i)) {
 		                			int temp = ham.get(i)+1;
@@ -185,7 +207,7 @@ public class BagOfWords {
 		            }
 		         }
 		         //initiate values
-		         bagS.setText(Integer.toString(spam.size()+ham.size()));
+		         bagS.setText(Integer.toString(tdSize));
 		         bagT.setText(Integer.toString(total));
 		         hamTotal.setText(Integer.toString(dSize2));
 		         for(String k : ham.keySet()){
@@ -194,20 +216,27 @@ public class BagOfWords {
 			}
 		});
 		
-		
+		//button to start spam filtering algorithm upon message input in the input textArea
 		JButton filter = new JButton("Filter");
 		filter.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				double upper = 1;
-           		double lower = 1;
+			    //variables needed
+				double upper;
+           		double lower;
 		        String user_inputs = input.getText();
                 String[] l = user_inputs.split("\n");
+            
                 if(user_inputs.length() == 0){ 
                     JOptionPane.showMessageDialog(new JFrame(),"There are no lines to identify!","Empty Input Area",JOptionPane.ERROR_MESSAGE);
                     return;
                 } 
                 
+                //getting everyline from the input textArea
+                //parsing every word to match every frequency
+                //calculate spam probability via every word in the message via naive bayes method   
                 for(String s : l) {
+                	upper = 1;
+                	lower = 1;
                 	String[] row = s.split(" ");
                 	for(String i : row){
                     	i = i.toLowerCase().replaceAll("[^A-Za-z0-9]","");
@@ -219,62 +248,47 @@ public class BagOfWords {
                 		//getting P(w|SPAM)
                 		if(spam.containsKey(i)){
                 			upper = upper * (spam.get(i)/(double)dSize1);
-                			System.out.println("P(w|Spam) :word="+i+"\n"+spam.get(i)+"/"+dSize1);
                 		} else {
                 			upper = 0;
-                			System.out.println("P(w|Spam) : 0/"+dSize1);
                 		}
                     	
                     	//getting P(w|HAM)
                     	if(ham.containsKey(i)){
                     		lower = lower * (ham.get(i)/(double)dSize2);
-                    		System.out.println("P(w|Ham) : word="+i+"\n"+ham.get(i)+"/"+dSize2);
                     	} else {
                     		lower = 0;
-                    		System.out.println("P(w|Ham) : 0/"+dSize2);
                     	}
                     }
-                    
-                    System.out.println("HSize="+hamLines);
-                    System.out.println("SSize="+spamLines);
+                    //final calculation of upper and lower halves
+                    //multiplication of probability of spam at upper
+                    //combination of (upper and lower) as resulting lower value 
                     upper = upper * (spamLines/(double)(spamLines+hamLines));
                     lower = lower * (hamLines/(double)(spamLines+hamLines));
                     lower = lower + upper;
+                    
+                    //evaluation of message if it is a HAM or a SPAM
                     if(upper==0){
-                        JOptionPane.showMessageDialog(new JFrame(),"Message is Ham!","Results!",JOptionPane.WARNING_MESSAGE);
                         mod3.addRow(new Object[]{s, "HAM"});
                         continue;
                     }
                     result = (double)upper/lower;
-                    System.out.println("Result is"+result);
                     if(result > 0.5) {
-                        //JOptionPane.showMessageDialog(new JFrame(),"Message is Spam!","Results!",JOptionPane.WARNING_MESSAGE);
                         mod3.addRow(new Object[]{s, "SPAM"});
                     }
                     else {
-                        JOptionPane.showMessageDialog(new JFrame(),"Message is Ham!","Results!",JOptionPane.WARNING_MESSAGE);
                         mod3.addRow(new Object[]{s, "HAM"});
                     }
                 }
 			}
 		});
 		
-		
-		//browseSpam
-		//table
-		//spamLabels
-		
-		//browseHam
-		//table2
-		//hamLabels
-		
-		//filter
+		//container for holding spam label displays/status
 	    Container spamStatus = new Container();
 	    spamStatus.setLayout(new FlowLayout(1,1,10));
 		spamStatus.add(spamLabel);
 		spamStatus.add(spamTotal);
         
-		
+        //container for spam table and partition
 		Container c1 = new Container();
 		c1.setLayout(new GridBagLayout());
 		GridBagConstraints config = new GridBagConstraints();
@@ -295,11 +309,13 @@ public class BagOfWords {
 	    config.gridy=2;
 		c1.add(spamStatus,config);
 		
+		//container for holding ham label displays/status
 		Container hamStatus = new Container();
 	    hamStatus.setLayout(new FlowLayout(1,1,10));
 		hamStatus.add(hamLabel);
 		hamStatus.add(hamTotal);
 		
+		//container for ham table and partition
 		Container c2 = new Container();
 		c2.setLayout(new GridBagLayout());
 		config.fill = GridBagConstraints.NONE;
@@ -320,7 +336,7 @@ public class BagOfWords {
 	    config.gridy=2;
 		c2.add(hamStatus,config);
 		
-		
+		//container for total dictionary size and word count status/display
 		Container status = new Container();
 		status.setLayout(new FlowLayout());
 		status.add(bagSize);
@@ -328,8 +344,7 @@ public class BagOfWords {
 		status.add(bagTotal);
 		status.add(bagT);
 				
-				
-		
+		//container for input and output section and partition
 		Container c3 = new Container();
 		c3.setLayout(new GridBagLayout());
 		config.weightx=0.5;
@@ -359,16 +374,12 @@ public class BagOfWords {
 		config.gridy=4;
 		c3.add(new JScrollPane(output),config); 
 		
-		mainFrame.setLayout(new GridLayout(1,3));
 		
+		//construction of whole UI
+		mainFrame.setLayout(new GridLayout(1,3));
 		mainFrame.add(c1);
 		mainFrame.add(c2);
 		mainFrame.add(c3);
-		//mainFrame.add(c4);
-		//mainFrame.add(c5);
-		//mainFrame.add(c6);
-		
-		
 		mainFrame.pack();
 		mainFrame.setVisible(true);
 		
